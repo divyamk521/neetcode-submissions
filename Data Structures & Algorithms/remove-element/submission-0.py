f43609096326f58a384fff1c9409e1128cc5d3c6
Divya M K <divyamk521@gmail.com>
2026-05-31 09:57:10 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        #Two pointers TC->O(N)  SC->O(1)
+        k=0
+        for  i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+
+                
+            
+        
